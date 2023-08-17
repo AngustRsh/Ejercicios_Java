1225826d9079855f6f75797cb4bb0bd20b50716a
AngustRsh <moyanonahuel@gmail.com>
2023-08-17 09:19:54 -0300

    js 17/08/23

---
+++ Guia18-JavaScript/Ejercicio01/js/code.js
@@ -0,0 +1,3 @@
+var dia = prompt("como esta el dia de hoy?(soleado, nublado, lluviendo)");
+var estado = "El dia de hoy esta " +dia;
+alert (estado);
---
+++ Guia18-JavaScript/Ejercicio03/js/Ejercicio03.js
@@ -0,0 +1,6 @@
+let edad = prompt("Ingrese edad: ");
+if(edad>=18){
+    alert("Es mayor de edad")
+}else{
+    alert("Es menor de edad")
+}
